7d068f35e611cd847a91532619fa3b4a5912d3e5
Yuji Nojima <59217497+Yujio5017@users.noreply.github.com>
2021-03-06 21:29:21 +0930

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ function App() {
   const [nameList,setnamesList]= useState([]);
   const [currentRound,setcurrentRound]= useState(1);
   const [pListSize, setpListSize] = useState(0);
-  const [qListSize, setqListSize] = useState(2);
-  const [timePerQuestion,setQTimer] = useState(15);
-  const [numRounds,setNumRounds] = useState(10);
+  const [qListSize, setqListSize] = useState(3);
+  const [timePerQuestion,setQTimer] = useState(30);
+  const [numRounds,setNumRounds] = useState(3);
   const [appearedPairs,setAppearedPairs] = useState([]);
-  
+  const [pack,setPack]= useState('default');
+
   const resetRounds = () =>{
     setcurrentRound(1);
     setAppearedPairs([]);
@@ -137,8 +138,22 @@ function App() {
           <Switch>
           <Route path="/" exact render={({match})=>(
          <Sidebar>
-    <form onSubmit={getName}>
-      <h1>Enter Name</h1>
+     <div className="rectangle" style={{zIndex:2}}>
+      <div style={{zIndex:1}}>
+      <img src="star.png" className="star"/>
+      <img src="squiggle.png" className="squiggle"/>
+      <img src="eclipse.png" className="eclipse"/>
+      </div>
+      <div className="logo">
+          <img src="get2know-logo.png" alt="get2know logo"/>
+      </div>
+        
+      <div class="motto">
+          <h1>Bringing people</h1>
+          <h1>together.</h1>
+      </div>
+        
+    <form class="entername" autoComplete="off" onSubmit={getName}>
 
       <input
         id="namesInput"
@@ -148,56 +163,86 @@ function App() {
         placeholder='Enter Name'
         className='border-rounded py-2'
       />
-      <button type='submit'>Send!</button>
+      <p className="small">hit enter to add</p>
     </form>
-      <Link to={"/lobby"}>
-      <button>Begin Game!</button>
-      </Link>
+        
+
       {nameList&&(
+        <div className=" player-container">
             <ul className="grid grid-col-5">
               {nameList.map(player =>(
                   <li key={player}>
-                    <div>
-                      <h1 className="styleas{player.color}">{player}</h1>
-                    </div> 
-                    <button onClick={(e) =>{clearName(player);handleClick(e);}}>Clear</button>
+                    {player}
+                    {/* <button onClick={(e) =>{clearName(player);handleClick(e);}}>Clear</button> */}
                   </li>
               ))}
             </ul>
-      )}
+            </div>
+      )}<div className="start-container">
+      <Link to={"/settings"}>
+        
+      		<p className="start">Settings &#8594;</p>
+      </Link>
+      <Link to={`/lobby`}>
+          <p className="custom-start">Start Game &#8594;</p>
+       
+      </Link> 
+ </div>
+    </div>
 
        </Sidebar>
           )}/>
-          <Route path="/lobby" render={()=>(
+          <Route path="/settings" render={()=>(
             
-            <div>
-              <h1>whatchu wanna do</h1>
+            <div className="outer-container-custom">
+              <img src="star3.png" className="custom-star"/>
+              <img src="semicircle.png" className="custom-semicircle"/>
+              <img src="ellipse.png" className="custom-ellipse"/>
+
+              <div className="custom-logo">
+                  <img src="get2know-logo.png" alt="get2know logo"/>
+              </div>
+
+              <div className="custom-container">
+                <div className="custom-tagline">
+                   <h1> Play it your way</h1>
+                </div>
+
+                <div className="custom-rectangle">
+                <div className="custom-settings">
             <form onChange={setSettings}>
-            <table>
-                <tr>
-                  <td>Time per question slider:</td>
-                  <td><input value={timePerQuestion} onChange={setTimer}type="range"  name="timePerQuestion" min="5" max="60" step="1"/></td>
-                  <td><span id="timeshow">15</span> Seconds</td>
-                </tr>
-                <tr>
-                  <td>Number of rounds slider:</td>
-                  <td><input value={numRounds} onChange={setRounds} type="range" name="numRounds" min="1" max="50" step="1"/></td>
-                  <td><span id="roundshow">10</span> Rounds</td>
-                </tr>
-                <tr>
-                  <td>Question pack dropdown:</td>
-                  <td>
-                  <select id="questionPackName" name="questionPackName">
-                    <option value="icebreakers">Icebreakers</option>
-                    <option value="nsfw">NSFW</option>
-                  </select>
-                  </td>
-                </tr>
-            </table>  
+            <div className="table">
+                <div>
+                  <p className="setting-names">Time per question slider:</p>
+                  <div><input value={timePerQuestion} onChange={setTimer}type="range"  name="timePerQuestion" min="5" max="61" step="1"/></div>
+                  <div><span id="timeshow">30</span> Seconds</div>
+                </div>
+                <div>
+                  <p className="setting-names">Number of rounds slider:</p>
+                  <div><input value={numRounds} onChange={setRounds} type="range" name="numRounds" min="1" max="11" step="1"/></div>
+                  <div><span id="roundshow">3</span> Rounds</div>
+                </div>
+                <div>
+                <p className="setting-names">Question pack dropdown:</p>
+                <div>
+                <select id="questionPackName" name="questionPackName">
+                  <option value="icebreakers">Icebreakers</option>
+                  <option value="nsfw">NSFW</option>
+                </select>
+                </div>
+              </div>
+            </div>  
             </form>
-            <Link to={`/round1`}>
-            <button onClick={(e) =>{newRound();getQuestion()}}>Begin Game!</button>
+            </div>
+            </div>
+            </div>
+
+            <Link to={`/lobby`}>
+              <div className="start-container">
+                <p className="custom-start" onClick={(e) =>{newRound();getQuestion()}}>Start Game &#8594;</p>
+              </div>
             </Link> 
+
             </div>
           )}/>
           <Route path="/round:id" render={()=>(
@@ -206,13 +251,53 @@ function App() {
             <button onClick={(e)=>resetRounds()}>back to lobbyu</button>
             </Link> 
               <h1>Round {currentRound-1}</h1>
-              <h1 id="questionHolder">enter question</h1>
+              <h1 id="questionHolder" style={{fontSize:'6.5vw', color:'white'}}>enter question</h1>
               <br/>
-              <h3 id="personHolder">name</h3>
+              <h3 id="personHolder" style={{fontSize:'4.5vw', color:'white'}}>name</h3>
               <Link to={`/round${currentRound}`}>
             <button onClick={(e) =>{newRound();getQuestion()}}>next round!</button>
             </Link> 
             </div>
+          )}/>
+          <Route path="/lobby" render={()=>(
+          <div className="activity-container">
+          <img src="polygon.png" className="activity-polygon"/>
+          <img src="star2.png" className="activity-star"/>
+          <img src="squiggle.png" className="activity-squiggle"/>
+          <div className="activity-logo">
+              <img src="get2know-logo.png" alt="get2know logo"/>
+          </div>
+          <div className="activity-header">
+              <h1>So...whatcha wanna do?</h1>
+          </div>
+  
+            <div className="activity-topics">
+                    <div className="activity-topic">
+                    <Link to={`/round1`}>
+                        <div onClick={(e) =>{newRound();setPack("default")}} className="activity-rectangle" id="box1"><img src="box.png"/></div>
+                        <p className="activity-content">Break the ice</p>
+                        </Link>
+                    </div>
+                    <div className="activity-topic">
+                    <Link to={`/round1`}>
+                        <div onClick={(e) =>{newRound();setPack("class")}} className="activity-rectangle" id="box3"><img src="bell.png"/></div>
+                        <p className="activity-content">Class is in session</p>
+                        </Link>
+                    </div>
+                    <div className="activity-topic">
+                    <Link to={`/round1`}>
+                        <div onClick={(e) =>{newRound();setPack("romance")}} className="activity-rectangle" id="box2"><img src="flower.png"/></div>
+                        <p className="activity-content">Budding romance &#128563;</p>
+                        </Link>
+                    </div>
+                    <div className="activity-topic">
+                    <Link to={`/round1`}>
+                        <div onClick={(e) =>{newRound();setPack("more")}} className="activity-rectangle" id="box4"><img src="file.png"/></div>
+                        <p className="activity-content">More packs</p>
+                        </Link>
+                    </div>
+            </div>
+      </div>  
           )}/>
           </Switch>
           </Main>
@@ -224,8 +309,7 @@ function App() {
 const Sidebar = (props) => (
   <div style={{
       height:'100vh',
-      overflow:'auto',
-      background:'#eee'
+      overflow:'auto'
   }} {...props}/>
   
 )
@@ -234,7 +318,11 @@ const Main = (props) => (
       flex:1,
       height:'100vh-60px',
       overflow:'auto',
-      background: '#ded3c9'
+      margin: 0,
+      fontFamily: "Poppins",
+      background: '#131313',
+      height: "100vh",
+      textAlign: "center",
   }}>
     <div  {...props}/>
     </div>
